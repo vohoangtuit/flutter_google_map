2e29828dd6594b573fef850450e931d36b1a07d6
Vo Hoang Tu <vht.developer@gmail.com>
2020-08-11 20:57:43 +0700

    setup google map in ios

--- google_map/ios/Runner/AppDelegate.swift
+++ google_map/ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCrdsRSWgms4aN4mcccQ2uzS2tUYqFv9Nk")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
